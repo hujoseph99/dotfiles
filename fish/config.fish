ffa0d3b3db0567d16dee96413faa671974022416
hujoseph99 <hujoseph99@gmail.com>
2025-03-01 16:00:29 +0200

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# environment variables
 set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+# aliases
+function gs
+    command git status
+end
 
 starship init fish | source
